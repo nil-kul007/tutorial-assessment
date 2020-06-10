0569f248583859d7191d4e9d1cdc77e2cc9dd629
nil-kul007 <nil.kul007@gmail.com>
2020-06-10 13:18:09 +0530

    Updates

---
+++ precache-manifest.6b8c482af9a103fc3685afb85c6fdc69.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54297417f5cfe3483f29def41982070",
+    "url": "/tutorial-assessment/index.html"
+  },
+  {
+    "revision": "97b444da19b78f1a7dc0",
+    "url": "/tutorial-assessment/static/css/main.8962c04a.chunk.css"
+  },
+  {
+    "revision": "a450dc6e9fe36b5c7aab",
+    "url": "/tutorial-assessment/static/js/2.092f0eae.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/tutorial-assessment/static/js/2.092f0eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d97e61df7df35fe3e9",
+    "url": "/tutorial-assessment/static/js/3.b60215be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tutorial-assessment/static/js/3.b60215be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ac863eccd5b8be461b",
+    "url": "/tutorial-assessment/static/js/4.b27093e0.chunk.js"
+  },
+  {
+    "revision": "97b444da19b78f1a7dc0",
+    "url": "/tutorial-assessment/static/js/main.44827dd7.chunk.js"
+  },
+  {
+    "revision": "a36bb2f43a378b56828a",
+    "url": "/tutorial-assessment/static/js/runtime-main.aa4fee61.js"
+  },
+  {
+    "revision": "7da92607f4d827e88310cf01947a8c85",
+    "url": "/tutorial-assessment/static/media/defauldImage.7da92607.png"
+  }
+]);
